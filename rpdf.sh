d7e3521b5fc364303e5f1d7302f5d578a0b2b249
Khan M Rashedun-Naby <naby88@gmail.com>
2018-02-26 09:12:13 +0600

    Script Updated

--- rpdf.sh
+++ rpdf.sh
@@ -2,15 +2,14 @@
 
 export ENSCRIPT="
 --portrait
---columns=2
---highlight=c
+--columns=1
 --color=1
 --margins=10:10:10:10
---font=Courier@3
+--font=Courier-Bold@7
 --borders
 --tabsize=2
 --line-numbers
+--highlight=c
 --output=-
-"
-
+" 
 iconv -f utf-8 -t iso-8859-1 "$1" | enscript --header=' '"$1"'|$n|%F  Page $% of $='
